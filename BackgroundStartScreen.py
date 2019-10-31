f27b994a779defc7ff2787c2963c5bb61f70d367
Bob1228022 <55795609+Bob1228022@users.noreply.github.com>
2019-10-31 19:45:21 -0400

    Update BackgroundStartScreen.py

--- BackgroundStartScreen.py
+++ BackgroundStartScreen.py
@@ -1,18 +1,17 @@
 
-start_x = 320
-        start_y = 140
+start_x = 275
+        start_y = 50
         #arcade.draw_point(start_x, start_y, arcade.color.BLUE, 5)
         arcade.draw_text("Tombstone", start_x, start_y, arcade.color.BLACK, 15, font_name='GARA')
-arcade.draw_rectangle_filled(400,130 , 300, 170, arcade.color.WHITE)
+arcade.draw_rectangle_filled(300,50 , 100, 50, arcade.color.WHITE)
 
-x = 300
-y = 300
-radius = 200
-arcade.draw_circle_filled(x, y, radius, arcade.color.ORANGE)
+arcade.draw_ellipse_filled(300, 300, 250, 150 arcade.color.ORANGE)
+arcade.draw_rectangle_filled(300, 475, 40, 50, arcade.color.BROWN)
 
-arcade.draw_triangle_filled(400,400,380,450, 360, 400 arcade.color.BLACK)
 
-arcade.draw_triangle_filled(200,400, 220, 450,240, 400 arcade.color.BLACK)
+arcade.draw_triangle_filled(400,400,360,450, 320, 400 arcade.color.BLACK)
+
+arcade.draw_triangle_filled(200,400, 240, 450,280, 400 arcade.color.BLACK)
 x = 300
 y = 280
 width = 120
@@ -21,3 +20,4 @@ start_angle = 190
 end_angle = 350
 arcade.draw_arc_outline(x, y, width, height, arcade.color.BLACK,
                         start_angle, end_angle, 10)
+
